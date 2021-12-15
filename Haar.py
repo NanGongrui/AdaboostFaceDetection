e4178c61b129eabacf51b4d8f5e0a980ac4a813c
NanGongrui <87226989+NanGongrui@users.noreply.github.com>
2021-12-15 18:51:44 +0800

    Add files via upload

--- Haar.py
+++ Haar.py
@@ -31,28 +31,215 @@ Haar特征
 '''
 
 def cal_feature1(integralimage, position):
-    m = int(position[1] + ((position[3] - position[1] + 1) / 2 - 1))
-    return 2 * integralimage[m, position[2]] - integralimage[position[3], position[2]]
+    x1 = position[0] - 1
+    y1 = position[1] - 1
+    x2 = position[2]
+    y2 = position[3]
+    m = int(y1 + (y2 - y1) / 2)
+    if x1 < 0 and y1 < 0:
+        ii1 = 0
+        ii2 = 0
+        ii3 = 0
+        ii4 = integralimage[m, x2]
+        ii5 = 0
+        ii6 = integralimage[y2, x2]
+    elif x1 < 0:
+        ii1 = 0
+        ii2 = integralimage[y1, x2]
+        ii3 = 0
+        ii4 = integralimage[m, x2]
+        ii5 = 0
+        ii6 = integralimage[y2, x2]
+    elif y1 < 0:
+        ii1 = 0
+        ii2 = 0
+        ii3 = integralimage[m, x1]
+        ii4 = integralimage[m, x2]
+        ii5 = integralimage[y2, x1]
+        ii6 = integralimage[y2, x2]
+    else:
+        ii1 = integralimage[y1, x1]
+        ii2 = integralimage[y1, x2]
+        ii3 = integralimage[m, x1]
+        ii4 = integralimage[m, x2]
+        ii5 = integralimage[y2, x1]
+        ii6 = integralimage[y2, x2]
+    return ii4-ii2-ii3+ii1-ii6+ii4+ii5-ii3
 
 def cal_feature2(integralimage, position):
-    m = int(position[0] + ((position[2] - position[0] + 1) / 2 - 1))
-    return 2 * integralimage[position[3], m] - integralimage[position[3], position[2]]
+    x1 = position[0] - 1
+    y1 = position[1] - 1
+    x2 = position[2]
+    y2 = position[3]
+    m = int(x1 + (x2 - x1) / 2)
+    if x1 < 0 and x2 < 0:
+        ii1 = 0
+        ii2 = 0
+        ii3 = 0
+        ii4 = 0
+        ii5 = integralimage[y2, m]
+        ii6 = integralimage[y2, x2]
+    elif x1 < 0:
+        ii1 = 0
+        ii2 = integralimage[y1, m]
+        ii3 = integralimage[y1, x2]
+        ii4 = 0
+        ii5 = integralimage[y2, m]
+        ii6 = integralimage[y2, x2]
+    elif y1 < 0:
+        ii1 = 0
+        ii2 = 0
+        ii3 = 0
+        ii4 = integralimage[y2, x1]
+        ii5 = integralimage[y2, m]
+        ii6 = integralimage[y2, x2]
+    else:
+        ii1 = integralimage[y1, x1]
+        ii2 = integralimage[y1, m]
+        ii3 = integralimage[y1, x2]
+        ii4 = integralimage[y2, x1]
+        ii5 = integralimage[y2, m]
+        ii6 = integralimage[y2, x2]
+    return ii5-ii2-ii4+ii1-ii6+ii3+ii5-ii2
 
 def cal_feature3(integralimage, position):
-    m1 = int(position[1] + ((position[3] - position[1] + 1) / 3 - 1))
-    m2 = int(position[1] + ((position[3] - position[1] + 1) * 2 / 3 - 1))
-    return 2 * integralimage[m1, position[2]] - 2 * integralimage[m2, position[2]] + integralimage[position[3], position[2]]
+    x1 = position[0] - 1
+    y1 = position[1] - 1
+    x2 = position[2]
+    y2 = position[3]
+    m1 = int(y1 + (y2 - y1) / 3)
+    m2 = int(y1 + (y2 - y1) / 3 * 2)
+    if x1 < 0 and y1 < 0:
+        ii1 = 0
+        ii2 = 0
+        ii3 = 0
+        ii4 = integralimage[m1, x2]
+        ii5 = 0
+        ii6 = integralimage[m2, x2]
+        ii7 = 0
+        ii8 = integralimage[y2, x2]
+    elif x1 < 0:
+        ii1 = 0
+        ii2 = integralimage[y1, x2]
+        ii3 = 0
+        ii4 = integralimage[m1, x2]
+        ii5 = 0
+        ii6 = integralimage[m2, x2]
+        ii7 = 0
+        ii8 = integralimage[y2, x2]
+    elif y1 < 0:
+        ii1 = 0
+        ii2 = 0
+        ii3 = integralimage[m1, x1]
+        ii4 = integralimage[m1, x2]
+        ii5 = integralimage[m2, x1]
+        ii6 = integralimage[m2, x2]
+        ii7 = integralimage[y2, x1]
+        ii8 = integralimage[y2, x2]
+    else:
+        ii1 = integralimage[y1, x1]
+        ii2 = integralimage[y1, x2]
+        ii3 = integralimage[m1, x1]
+        ii4 = integralimage[m1, x2]
+        ii5 = integralimage[m2, x1]
+        ii6 = integralimage[m2, x2]
+        ii7 = integralimage[y2, x1]
+        ii8 = integralimage[y2, x2]
+    return ii4-ii2-ii3+ii1-ii6+ii4+ii5-ii3+ii8-ii6-ii7+ii5
 
 def cal_feature4(integralimage, position):
-    m1 = int(position[0] + ((position[2] - position[0] + 1) / 3 - 1))
-    m2 = int(position[0] + ((position[2] - position[0] + 1) * 2 / 3 - 1))
-    return 2 * integralimage[position[3], m1] - 2 * integralimage[position[3], m2] + integralimage[position[3], position[2]]
+    x1 = position[0] - 1
+    y1 = position[1] - 1
+    x2 = position[2]
+    y2 = position[3]
+    m1 = int(x1 + (x2 - x1) / 3)
+    m2 = int(x1 + (x2 - x1) / 3 * 2)
+    if x1 < 0 and y1 < 0:
+        ii1 = 0
+        ii2 = 0
+        ii3 = 0
+        ii4 = 0
+        ii5 = 0
+        ii6 = integralimage[y2, m1]
+        ii7 = integralimage[y2, m2]
+        ii8 = integralimage[y2, x2]
+    elif x1 < 0:
+        ii1 = 0
+        ii2 = integralimage[y1, m1]
+        ii3 = integralimage[y1, m2]
+        ii4 = integralimage[y1, x2]
+        ii5 = 0
+        ii6 = integralimage[y2, m1]
+        ii7 = integralimage[y2, m2]
+        ii8 = integralimage[y2, x2]
+    elif y1 < 0:
+        ii1 = 0
+        ii2 = 0
+        ii3 = 0
+        ii4 = 0
+        ii5 = integralimage[y2, x1]
+        ii6 = integralimage[y2, m1]
+        ii7 = integralimage[y2, m2]
+        ii8 = integralimage[y2, x2]
+    else:
+        ii1 = integralimage[y1, x1]
+        ii2 = integralimage[y1, m1]
+        ii3 = integralimage[y1, m2]
+        ii4 = integralimage[y1, x2]
+        ii5 = integralimage[y2, x1]
+        ii6 = integralimage[y2, m1]
+        ii7 = integralimage[y2, m2]
+        ii8 = integralimage[y2, x2]
+    return ii6-ii2-ii5+ii1-ii7+ii3+ii6-ii2+ii8-ii4-ii7+ii3
 
 def cal_feature5(integralimage, position):
-    my = int(position[1] + ((position[3] - position[1] + 1) / 2 - 1))
-    mx = int(position[0] + ((position[2] - position[0] + 1) / 2 - 1))
-    return 4 * integralimage[my, mx] - 2 * integralimage[my, position[2]] - 2 * integralimage[position[3], mx] + integralimage[position[3], position[2]]
-
+    x1 = position[0] - 1
+    y1 = position[1] - 1
+    x2 = position[2]
+    y2 = position[3]
+    mx = int(x1 + (x2 - x1) / 2)
+    my = int(y1 + (y2 - y1) / 2)
+    if x1 < 0 and y1 < 0:
+        ii1 = 0
+        ii2 = 0
+        ii3 = 0
+        ii4 = 0
+        ii5 = integralimage[my, mx]
+        ii6 = integralimage[my, x2]
+        ii7 = 0
+        ii8 = integralimage[y2, mx]
+        ii9 = integralimage[y2, x2]
+    elif x1 < 0:
+        ii1 = 0
+        ii2 = integralimage[y1, mx]
+        ii3 = integralimage[y1, x2]
+        ii4 = 0
+        ii5 = integralimage[my, mx]
+        ii6 = integralimage[my, x2]
+        ii7 = 0
+        ii8 = integralimage[y2, mx]
+        ii9 = integralimage[y2, x2]
+    elif y1 < 0:
+        ii1 = 0
+        ii2 = 0
+        ii3 = 0
+        ii4 = integralimage[my, x1]
+        ii5 = integralimage[my, mx]
+        ii6 = integralimage[my, x2]
+        ii7 = integralimage[y2, x1]
+        ii8 = integralimage[y2, mx]
+        ii9 = integralimage[y2, x2]
+    else:
+        ii1 = integralimage[y1, x1]
+        ii2 = integralimage[y1, mx]
+        ii3 = integralimage[y1, x2]
+        ii4 = integralimage[my, x1]
+        ii5 = integralimage[my, mx]
+        ii6 = integralimage[my, x2]
+        ii7 = integralimage[y2, x1]
+        ii8 = integralimage[y2, mx]
+        ii9 = integralimage[y2, x2]
+    return ii5-ii2-ii4+ii1-ii6+ii3+ii5-ii2-ii8+ii5+ii7-ii4+ii9-ii6-ii8+ii5
 
 def cal_feature(integralimage, position, type):
     if type == 1:
@@ -75,4 +262,4 @@ if __name__ == '__main__':
     print(img.shape)
     integralimage = GetIntegralImage(img1)
     print(integralimage)
-    print(cal_feature(integralimage, [0, 0, 2, 2], 5))
+    print(cal_feature(integralimage, [1, 1, 3, 3], 4))
